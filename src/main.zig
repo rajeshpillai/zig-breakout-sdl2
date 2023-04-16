e88ae01e880f6ef2680180026109a7798dde613f
Rajesh Pillai <rajesh@algorisys.com>
2023-04-16 09:28:44 +0530

    Draw and move rect across screen

--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,18 @@ pub fn main() !void {
     defer c.SDL_DestroyRenderer(renderer);
 
     var quit = false;
+
+    const FPS = 60;
+
+    const DELTA_TIME_SEC: f32 = 1.0 / @intToFloat(f32, FPS);
+    const SPEED: f32 = 100;
+
+    var x: f32 = 0;
+    var y: f32 = 0;
+
+    var dx:f32 = 1;
+    var dy:f32 = 1;
+
     while (!quit) {
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
@@ -31,9 +43,27 @@ pub fn main() !void {
             }
         }
 
+        
         _ = c.SDL_SetRenderDrawColor(renderer, 0x18, 0x18, 0x18, 0xFF);
         _ = c.SDL_RenderClear(renderer);
+        
+
+        x += dx * SPEED * DELTA_TIME_SEC;
+        y += dy * SPEED * DELTA_TIME_SEC;
+
+        const rect = c.SDL_Rect {
+            .x = @floatToInt(i32, x),
+            .y = @floatToInt(i32, y),
+            .w = 100,
+            .h = 100
+        };
+
+        _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, 0xFF);
+        _ = c.SDL_RenderFillRect(renderer, &rect);
+        
         c.SDL_RenderPresent(renderer);
+        c.SDL_Delay(1000 / FPS);
+        
     }
 
 
