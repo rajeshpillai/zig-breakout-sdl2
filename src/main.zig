3731f32f2a6c93458f757d6a84dc7cfa763e1ac8
Rajesh Pillai <rajesh@algorisys.com>
2023-04-16 11:04:27 +0530

    Refactor

--- src/main.zig
+++ src/main.zig
@@ -21,6 +21,62 @@ const Point = struct {
     proj_y: f32
 };
 
+var quit = false;
+var proj_x: f32 = 100;
+var proj_y: f32 = 100;
+var proj_dx:f32 = 1;
+var proj_dy:f32 = 1;
+var bar_x: f32 = 100;
+var bar_dx: f32 = 0;
+var pause = false;
+
+
+fn render(renderer: *c.SDL_Renderer )  void  {
+    _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    const proj_rect = c.SDL_Rect {
+        .x = @floatToInt(i32, proj_x),
+        .y = @floatToInt(i32, proj_y),
+        .w = PROJ_SIZE,
+        .h = PROJ_SIZE
+    };
+
+    _ = c.SDL_RenderFillRect(renderer, &proj_rect);
+
+    _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, 0xFF);
+    const bar_rect = c.SDL_Rect {
+        .x = @floatToInt(i32, bar_x),
+        .y = @floatToInt(i32, BAR_Y - BAR_THICKNESS / 2),
+        .w = @floatToInt(i32, BAR_LEN),
+        .h = @floatToInt(i32, BAR_THICKNESS)
+    };
+
+    _ = c.SDL_RenderFillRect(renderer, &bar_rect);
+}
+
+fn update(dt: f32)  void  {
+    if (!pause) {
+        bar_x += bar_dx *  BAR_SPEED * dt;
+        
+        var proj_nx = proj_x + proj_dx * PROJ_SPEED * dt;
+
+        if (proj_nx < 0 or proj_nx + PROJ_SIZE > WINDOW_WIDTH) {
+            proj_dx *= -1;
+            proj_nx = proj_x + proj_dx * PROJ_SPEED * dt;
+        }
+
+        var proj_ny = proj_y + proj_dy * PROJ_SPEED * dt;
+
+        if (proj_ny < 0 or proj_ny + PROJ_SIZE > WINDOW_HEIGHT) {
+            proj_dy *= -1;
+            proj_ny = proj_y + proj_dy * PROJ_SPEED * dt;
+        }
+        
+        proj_x = proj_nx;
+        proj_y = proj_ny;
+    }
+}
+
+
 pub fn main() !void {
      if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
         c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
@@ -39,18 +95,6 @@ pub fn main() !void {
     defer c.SDL_DestroyRenderer(renderer);
 
     const keyboard = c.SDL_GetKeyboardState(null);
-
-    var quit = false;
-
-    var proj_x: f32 = 100;
-    var proj_y: f32 = 100;
-    var proj_dx:f32 = 1;
-    var proj_dy:f32 = 1;
-
-    var bar_x: f32 = 100;
-    var bar_dx: f32 = 0;
-
-    var pause = false;
 
     while (!quit) {
         var event: c.SDL_Event = undefined;
@@ -85,52 +129,13 @@ pub fn main() !void {
         }
         
 
-        if (!pause) {
-            bar_x += bar_dx *  BAR_SPEED * DELTA_TIME_SEC;
-            
-            var proj_nx = proj_x + proj_dx * PROJ_SPEED * DELTA_TIME_SEC;
-
-            if (proj_nx < 0 or proj_nx + PROJ_SIZE > WINDOW_WIDTH) {
-                proj_dx *= -1;
-                proj_nx = proj_x + proj_dx * PROJ_SPEED * DELTA_TIME_SEC;
-            }
-
-            var proj_ny = proj_y + proj_dy * PROJ_SPEED * DELTA_TIME_SEC;
-
-            if (proj_ny < 0 or proj_ny + PROJ_SIZE > WINDOW_HEIGHT) {
-                proj_dy *= -1;
-                proj_ny = proj_y + proj_dy * PROJ_SPEED * DELTA_TIME_SEC;
-            }
-            
-            proj_x = proj_nx;
-            proj_y = proj_ny;
-        }
-
+        update(DELTA_TIME_SEC);
 
         _ = c.SDL_SetRenderDrawColor(renderer, 0x18, 0x18, 0x18, 0xFF);
         _ = c.SDL_RenderClear(renderer);
-        
 
-        _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        
-        const proj_rect = c.SDL_Rect {
-            .x = @floatToInt(i32, proj_x),
-            .y = @floatToInt(i32, proj_y),
-            .w = PROJ_SIZE,
-            .h = PROJ_SIZE
-        };
+        render(renderer);
 
-        _ = c.SDL_RenderFillRect(renderer, &proj_rect);
-
-        _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, 0xFF);
-        const bar_rect = c.SDL_Rect {
-            .x = @floatToInt(i32, bar_x),
-            .y = @floatToInt(i32, BAR_Y - BAR_THICKNESS / 2),
-            .w = @floatToInt(i32, BAR_LEN),
-            .h = @floatToInt(i32, BAR_THICKNESS)
-        };
-
-        _ = c.SDL_RenderFillRect(renderer, &bar_rect);
         
         c.SDL_RenderPresent(renderer);
         c.SDL_Delay(1000 / FPS);
