4eec4f15fbad3b944a28412fc81dc85d0cafa4bc
Rajesh Pillai <rajesh@algorisys.com>
2023-04-16 10:24:32 +0530

    Draw bar

--- src/main.zig
+++ src/main.zig
@@ -12,8 +12,8 @@ const FPS = 60;
 const DELTA_TIME_SEC: f32 = 1.0 / @intToFloat(f32, FPS);
 
 const BAR_LEN = 100;
-const BAR_THICKNESS = 10;
-const BAR_proj_y = WINDOW_HEIGHT - BAR_THICKNESS - 100;
+const BAR_THICKNESS: f32 = 10;
+const BAR_Y: f32 = WINDOW_HEIGHT - BAR_THICKNESS - 100;
 
 const Point = struct {
     proj_x: f32,
@@ -41,8 +41,10 @@ pub fn main() !void {
 
     var proj_x: f32 = 100;
     var proj_y: f32 = 100;
-    var dproj_x:f32 = 1;
-    var dproj_y:f32 = 1;
+    var dx:f32 = 1;
+    var dy:f32 = 1;
+
+    var bar_x: f32 = 100;
 
     while (!quit) {
         var event: c.SDL_Event = undefined;
@@ -59,33 +61,44 @@ pub fn main() !void {
         _ = c.SDL_SetRenderDrawColor(renderer, 0x18, 0x18, 0x18, 0xFF);
         _ = c.SDL_RenderClear(renderer);
         
-        var proj_nx = proj_x + dproj_x * PROJ_SPEED * DELTA_TIME_SEC;
+        var proj_nx = proj_x + dx * PROJ_SPEED * DELTA_TIME_SEC;
 
         if (proj_nx < 0 or proj_nx + PROJ_SIZE > WINDOW_WIDTH) {
-            dproj_x *= -1;
-            proj_nx = proj_x + dproj_x * PROJ_SPEED * DELTA_TIME_SEC;
+            dx *= -1;
+            proj_nx = proj_x + dx * PROJ_SPEED * DELTA_TIME_SEC;
         }
 
-        var proj_ny = proj_y + dproj_y * PROJ_SPEED * DELTA_TIME_SEC;
+        var proj_ny = proj_y + dy * PROJ_SPEED * DELTA_TIME_SEC;
 
         if (proj_ny < 0 or proj_ny + PROJ_SIZE > WINDOW_HEIGHT) {
-            dproj_y *= -1;
-            proj_ny = proj_y + dproj_y * PROJ_SPEED * DELTA_TIME_SEC;
+            dy *= -1;
+            proj_ny = proj_y + dy * PROJ_SPEED * DELTA_TIME_SEC;
         }
 
         proj_x = proj_nx;
         proj_y = proj_ny;
         
 
-        const rect = c.SDL_Rect {
+        _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        
+        const proj_rect = c.SDL_Rect {
             .x = @floatToInt(i32, proj_x),
             .y = @floatToInt(i32, proj_y),
             .w = PROJ_SIZE,
             .h = PROJ_SIZE
         };
 
+        _ = c.SDL_RenderFillRect(renderer, &proj_rect);
+
         _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0, 0, 0xFF);
-        _ = c.SDL_RenderFillRect(renderer, &rect);
+        const bar_rect = c.SDL_Rect {
+            .x = @floatToInt(i32, bar_x),
+            .y = BAR_Y - BAR_THICKNESS / 2,
+            .w = BAR_LEN,
+            .h = BAR_THICKNESS
+        };
+
+        _ = c.SDL_RenderFillRect(renderer, &bar_rect);
         
         c.SDL_RenderPresent(renderer);
         c.SDL_Delay(1000 / FPS);
