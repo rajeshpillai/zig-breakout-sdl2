ac61d4047a48ddaad8c2d10405a5429165a8d4ff
Rajesh Pillai <rajesh@algorisys.com>
2023-04-16 08:55:54 +0530

    Show a window

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,30 @@ pub fn main() !void {
         return error.SDLInitializationFailed;
     }
     defer c.SDL_Quit();
+
+    var window = c.SDL_CreateWindow("Breakout Game", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, 640, 400, 0);
+    defer c.SDL_DestroyWindow(window);
+
+    var renderer = c.SDL_CreateRenderer(window, 0, c.SDL_RENDERER_PRESENTVSYNC) orelse {
+        c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
+        return error.SDLRendererCreationFailed;
+    };
+    defer c.SDL_DestroyRenderer(renderer);
+
+    var quit = false;
+    while (!quit) {
+        var event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&event) != 0) {
+            switch (event.@"type") {
+                c.SDL_QUIT => {
+                    quit = true;
+                },
+                else => {},
+            }
+        }
+    }
+
+
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 }
